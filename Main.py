e2f9b5f18a11ac70b1d21fdfebd5ec36b2cb1c21
ocorn2023 <161055450+ocorn2023@users.noreply.github.com>
2024-03-18 17:15:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,40 @@
 
+#2X82F3QM NW9Q66CH V27XQ4UK 94UYAJD6 9RTFZ4BX M7PA4SEG YTSFMS66 7VDS2BNH WKF8HVEJ 4TA9BGT8 P8CTSF4M TQEYKEM8 VZP2XYYN C2EYTDPC 2ZSBV293 
+#MZYGTEQE WLQTNH7P QDNWLRXP 8Q8JR9F4 4DUTC6DM NBXP3RGK 2WMHKUAH L3U27TH9 FGB4M8HR L5V64V7U DPW2Q5QN E48ZJRYF S96VKXPC 6JNSMMAZ 397E3D5K H6C8A7B9
+#CM56ZGFR 9MFCA3PH 69MC32Q6 G2UBX66E VHFC72XL KG2LFML7 PKP5T8Y8 2Q529FQR WFY49MML JWAKBES7 PGGTB6YU EXNQXT8H FYCW375F 3MUS3HDT ZLW24KJB EXN7UD38
+#VGYUJLHQ PCNKD5BA C5EUL45Q SAE9XUQP A9T6PVNJ KFUB2WCG 9JYD3PLL LET8NR88 2B59T7LD HAHU7FXR XUTZQMAY 822QDZPM 2J8KBQNS 2GNT24G9 HXVJ6BHF YLNKQB7X
+#PYRLTRF4 QY2C6PGM EZNKMVCE 4SZHG9FJ WVNJX99R ACKFXESF JTLP72XZ T7NQKUWV V99W5T65 FJCYQ6EQ ZKHB4KLG FKBN84VJ LCQ5HHRH TLT7B2SJ SUAZUF8X 8T4XL
+
+import tkinter as tk
+from time import sleep
+Print("Hello welcome to SaveMeMath. Input you lesson code and get a generated google document")
+print(" ")
+assing = input("What Is The Assignment code? (grade, *ac for acc*, unit, : ,lesson).           # ")
+
+def task():
+    # The window will stay open until this function call ends.
+    sleep(6) # Replace this with the code you want to run
+    root.destroy()
+
+root = tk.Tk()
+root.title("loading")
+
+label = tk.Label(root, text="Sanning files for your request")
+label.pack()
+
+root.after(200, task)
+root.mainloop()
+
+print("compiling data...")
+sleep(1)
+print("1/3")
+sleep(3)
+print("2/3")
+sleep(7)
+print("3/3")
+print(" file/jason.compile/google.com.doc/id:", assing, " .savememath/html.file/load")
+sleep(1)
+print(" send.file")
+
+if assing == 7ac5:1:
+  print(" here is your link to the document. :  https://google.com/ ")
